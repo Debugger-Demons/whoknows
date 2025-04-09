0757a392ec2601fe5d2954f40f6a35474a3d0fb9
Kevin Strandberg <kevin@npstrandberg.comm>
2025-04-09 17:00:56 +0200

    migration file should now be done

---
+++ backend/db-migration/migrations/20250409141333_create_intial_schema.cjs
@@ -0,0 +1,36 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", function (table) {
+      table.increments("id");
+      table.string("username").notNullable().unique();
+      table.string("email").notNullable().unique();
+      table.string("password").notNullable();
+    })
+    .then(() => {
+      return knex.schema.createTable("pages", function (table) {
+        table.string("title").primary().unique();
+        table.string("url").notNullable().unique();
+        table
+          .string("language")
+          .notNullable()
+          .defaultTo("en")
+          .checkIn(["en", "da"]);
+        table.timestamp("last_updated");
+        table.text("content").notNullable();
+      });
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("pages").then(() => {
+    return knex.schema.dropTableIfExists("users");
+  });
+};
