1a4d05b96615739c6e145d425eec51dfc666807c
Kevin Strandberg <kevin@npstrandberg.comm>
2025-04-09 14:40:05 +0200

    Removed .nodemodule from directory, and reverted SQL file to sql lite format.

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT NOT NULL UNIQUE,
   email TEXT NOT NULL UNIQUE,
   password TEXT NOT NULL
@@ -12,12 +12,10 @@ INSERT INTO users (username, email, password)
     VALUES ('admin', 'keamonk1@stud.kea.dk', '5f4dcc3b5aa765d61d8327deb882cf99');
 
 
-DROP TABLE IF EXISTS pages;
-
 CREATE TABLE IF NOT EXISTS pages (
-    title TEXT PRIMARY KEY,
+    title TEXT PRIMARY KEY UNIQUE,
     url TEXT NOT NULL UNIQUE,
-    language TEXT NOT NULL DEFAULT 'en' CHECK (language IN ('en', 'da')),
+    language TEXT NOT NULL CHECK(language IN ('en', 'da')) DEFAULT 'en', -- How you define ENUM type in SQLite
     last_updated TIMESTAMP,
     content TEXT NOT NULL
 );
