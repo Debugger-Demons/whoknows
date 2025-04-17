aefd4dc9ada5d520601f7e717c3bb34ce7c7a5f8
AlekOmOm <AlekOmOm@users.noreply.github.com>
2025-04-17 16:13:03 +0200

    Refactor Dockerfile and environment configurations for improved clarity and functionality
    - Updated the backend Dockerfile to separate the build and production stages, enhancing the image's efficiency and maintainability.
    - Added local image tags for backend and frontend in the .env.development file to facilitate local development.
    - Modified Makefile.toml for both backend and frontend to reflect the new environment variable usage and improve the development workflow.
    - Updated .gitignore files to include new environment files, ensuring sensitive information is excluded from version control.

---
+++ backend/scripts/dev_docker.py
@@ -0,0 +1,56 @@
+"""
+   Builds and runs the Docker container for local development.
+   - using .env.local.frontend 
+   running: 
+   'docker build -t ${COMPOSE_PROJECT_NAME} . 
+      && docker run --rm -d 
+         --env-file .env.local.frontend 
+         -p ${FRONTEND_INTERNAL_PORT}:${FRONTEND_EXTERNAL_PORT} 
+         ${COMPOSE_PROJECT_NAME}'
+"""
+import sys
+import subprocess
+import makefile_env_loader
+
+PROJECT_NAME = makefile_env_loader.get_project_name()
+CONTAINER_NAME = f"{PROJECT_NAME}"
+FRONTEND_PORT = makefile_env_loader.get_frontend_port()
+BACKEND_PORT = makefile_env_loader.get_backend_port()
+
+def get_build_cmd(project_name_prm="whoknows.frontend.test") -> str | None:
+   """ docker build -t ${project_name} . """
+   return f"docker build -t {project_name_prm} ."
+
+def get_run_cmd(backend_port_prm="8080", image_name="whoknows.backend.test") -> str | None:
+   """ 
+   docker run --rm -d 
+         --env-file .env.local.backend 
+         -p ${BACKEND_INTERNAL_PORT}:${BACKEND_EXTERNAL_PORT} 
+         ${COMPOSE_PROJECT_NAME} 
+   """
+   return f"docker run --rm -d --env-file .env.local.backend -p {backend_port_prm}:{backend_port_prm} {image_name}"
+
+cmds = {
+   "build": get_build_cmd(PROJECT_NAME),
+   "run": get_run_cmd(BACKEND_PORT, CONTAINER_NAME)
+}
+
+def cmd_build_run_combined():
+   """ combined docker build && docker run """
+   return f"{cmds["build"]} && {cmds['run']}"
+
+def run_dev_docker():
+   """ executing the dev-docker commands for building and running container with env variables """
+   subprocess.run(cmd_build_run_combined(), shell=True, check=False)
+
+if __name__ == "__main__":
+   if not makefile_env_loader.check_env():
+      print(" \n"
+            "Error: Required environment file .env.local.frontend not found.\n"
+            "-> copy and edit the template: \n"
+            "  cp .env.local.frontend.template .env.local.frontend \n"
+            " \n",
+            file=sys.stderr)
+      sys.exit(1)
+      
+   run_dev_docker()
