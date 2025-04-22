8823e106e8b960139056b53be8af1d4dc4da86e9
AlekOmOm <AlekOmOm@users.noreply.github.com>
2025-04-22 20:12:56 +0200

    Enhance check_env script to validate required files for environment setup
    - Introduced a new function, `check_required_files`, to verify the existence of essential files, including `.env`, `.env.development`, and the database file.
    - Updated the `check_env` function to call `check_required_files`, improving user guidance by ensuring all necessary files are present before proceeding.
    - These changes enhance the robustness of the environment setup process, supporting ongoing efforts to streamline configuration management and improve maintainability.

--- scripts/check_env.py
+++ scripts/check_env.py
@@ -3,9 +3,18 @@ import os
 import sys
 
 ENV_FILE_PATH = ".env"
+REQUIRED_FILES = [
+            "./database/whoknows.db",
+            ".env",
+            ".env.development"
+]
 
 def check_env():
     """ Check if the .env file exists and print the contents """
+    
+    # check for required files
+    check_required_files()
+
     if not os.path.exists(ENV_FILE_PATH):
         print(f"Error: {ENV_FILE_PATH} does not exist")
         print("Please create a .env file in the root directory")
@@ -20,6 +29,18 @@ def check_env():
     with open(ENV_FILE_PATH, "r", encoding="utf-8") as file:
         for line in file:
             print(line.strip())
+
+def check_required_files():
+    """ Check if required files exist """
+    current_dir = os.getcwd()
+    print(f"Current directory: {current_dir}")
+    for file in REQUIRED_FILES:
+        if not os.path.exists(file):
+            print(f"Error: {file} does not exist")
+            print("Please create the required files")
+            sys.exit(1)
+        else:
+            print(f"{file} exists")
 
 def cp_env():
     """ get OS then proper CP command execution (Windows/Linux) """
