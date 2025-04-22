30090a7dde9710cc2f2a32f4c4156c55a88dca30
AlekOmOm <AlekOmOm@users.noreply.github.com>
2025-04-22 15:42:48 +0200

    Enhance check_env script for improved user guidance and environment setup
    - Updated the `check_env` function to provide clearer instructions when the `.env` file is missing, prompting users to create it interactively.
    - Introduced a new `cp_env` function to handle the copying of the `.env.development` file to `.env`, supporting both Windows and Linux environments.
    - These changes improve the usability of the script, ensuring developers can easily set up their local environment and reducing potential configuration errors.

--- scripts/check_env.py
+++ scripts/check_env.py
@@ -8,11 +8,25 @@ def check_env():
     """ Check if the .env file exists and print the contents """
     if not os.path.exists(ENV_FILE_PATH):
         print(f"Error: {ENV_FILE_PATH} does not exist")
-        sys.exit(1)
+        print("Please create a .env file in the root directory")
+        print("press Enter to create (cp .env.development .env) [y]/n)")
+        if input() != "n":
+            os.system("cd ..")
+            cp_env()
+            sys.exit(0)
+        else:
+            sys.exit(1)
 
     with open(ENV_FILE_PATH, "r", encoding="utf-8") as file:
         for line in file:
             print(line.strip())
+
+def cp_env():
+    """ get OS then proper CP command execution (Windows/Linux) """
+    if sys.platform == "win32":
+        os.system(f"copy .env.development {ENV_FILE_PATH}")
+    else: # Linux
+        os.system(f"cp .env.development {ENV_FILE_PATH}")
 
 if __name__ == "__main__":
     check_env()
